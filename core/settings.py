e5d5622d921d03e98810d0fb0ee4cac71a172399
marshDesigns <72623348+marshDesigns@users.noreply.github.com>
2022-09-02 20:38:19 +0100

    added suppliers home

--- core/settings.py
+++ core/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static', 'media'),]
 
