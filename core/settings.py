58380c4e8bd3a03fe97da7ee210e9e96d03d171d
marshDesigns <72623348+marshDesigns@users.noreply.github.com>
2022-09-03 13:55:11 +0100

    commit

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
--- core/urls.py
+++ core/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', include('dashboard.urls') ),
     path('', include('users.urls') ),
 ]
-static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
